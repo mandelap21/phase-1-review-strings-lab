4c167f1aded791093e9080d19ba8db174a3f4f73
philemon <mandelap21@gmail.com>
2024-06-27 22:36:23 +0300

    Second Assignment

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 const currentUser = 'Grace Hopper';
-
-// Create welcomeMessage
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-
-// Create excitedWelcomeMessage
 const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
-
-// Create shortGreeting
 const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
-
-module.exports = { currentUser, welcomeMessage, excitedWelcomeMessage, shortGreeting };
